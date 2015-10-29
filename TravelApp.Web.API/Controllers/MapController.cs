1af3c4025a3596aee3ced53954d9fba607718d9c
dradkins <dradkins11@gmail.com>
2015-10-29 17:28:54 +0500

    fixes

--- TravelApp.Web.API/Controllers/MapController.cs
+++ TravelApp.Web.API/Controllers/MapController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,8 +15,8 @@ namespace TravelApp.Web.API.Controllers
     {
         [HttpPost]
         [AllowAnonymous]
-        [Route("GetDistance")]
-        public IHttpActionResult GetDistance(LocationModel model)
+        [Route("GetAirDistance")]
+        public IHttpActionResult GetAirDistance(LocationModel model)
         {
             try
             {
@@ -29,7 +30,7 @@ namespace TravelApp.Web.API.Controllers
                 var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(Rad(l1lat)) * Math.Cos(Rad(l2lat)) * Math.Sin(dLong / 2) * Math.Sin(dLong / 2);
                 var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
                 var d = R * c;
-                return Ok(d/1000);
+                return Ok(d / 1000);
             }
             catch (DivideByZeroException)
             {
@@ -38,6 +39,34 @@ namespace TravelApp.Web.API.Controllers
             catch (InvalidCastException)
             {
                 return BadRequest("Please enter valid double values");
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("GetBestTravelDistance")]
+        public async Task<IHttpActionResult> GetBestTravelDistance(LocationModel model)
+        {
+            try
+            {
+                var l1lat = model.Location1Latitude;
+                var l1long = model.Location1Longitude;
+                var l2lat = model.Location2Latitude;
+                var l2long = model.Location2Longitude;
+                var _address = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + l1lat + "," + l1long + "&destinations=" + l2lat + "," + l2long + "&key=AIzaSyBsgN6RY1RbVU51gNq8RwwQj7ro-TaNMO0";
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(_address);
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Check that response was successful or throw exception
+                response.EnsureSuccessStatusCode();
+
+                JObject json = JObject.Parse(responseBody);
+                return Ok(json);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Some error occured during processing your request");
             }
         }
 
