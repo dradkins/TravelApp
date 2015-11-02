ea61de780b875afc701d09bc8cc78f64c8dd0c72
dradkins <dradkins11@gmail.com>
2015-11-02 21:46:16 +0500

    Fixes

---
+++ TravelApp.Models/GetDistanceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.Models
+{
+    public class GetDistanceModel
+    {
+        public string Location1Latitude { get; set; }
+        public string Location1Longitude { get; set; }
+        public string Location2Latitude { get; set; }
+        public string Location2Longitude { get; set; }
+    }
+}
---
+++ TravelApp/Helpers/ServiceHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using TravelApp.Models;
+
+namespace TravelApp.Helpers
+{
+    public class ServiceHelper
+    {
+        public static TravelInfoModel GetInfo(GetDistanceModel model)
+        {
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://travelapp.xivtech.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.PostAsJsonAsync("api/map/GetBestTravelDistance", model).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    RootObject obj = response.Content.ReadAsAsync<RootObject>().Result;
+                    TravelInfoModel returnModel = new TravelInfoModel(obj);
+                    return returnModel;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
